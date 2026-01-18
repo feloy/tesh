02b31584ea67cabf67e614f483a4ce9e2109a2c5
Philippe Martin <phmartin@redhat.com>
2026-01-18 18:07:24 +0100

    refactor: move Script to script.go

--- pkg/run/scenarios.go
+++ pkg/run/scenarios.go
@@ -12,23 +12,6 @@ import (
 	"mvdan.cc/sh/v3/syntax"
 )
 
-func Script(file *os.File) {
-	script, _ := syntax.NewParser().Parse(file, "")
-
-	runner, _ := interp.New(
-		interp.Env(expand.ListEnviron("GLOBAL=global_value")),
-		interp.Env(expand.ListEnviron(os.Environ()...)),
-		interp.StdIO(nil, os.Stdout, os.Stderr),
-	)
-	result := runner.Run(context.TODO(), script)
-
-	if status, ok := result.(interp.ExitStatus); ok {
-		os.Exit(int(status))
-	} else {
-		os.Exit(1)
-	}
-}
-
 func Scenarios(file *os.File, scenariosFile *os.File, singleScenarioID *string) {
 	if singleScenarioID == nil {
 		log.Fatal("single scenario ID is required")
