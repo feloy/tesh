d8bc73119f1a1abd15d62918f599bca0ed9b6870
Philippe Martin <feloy1@gmail.com>
2026-01-30 09:16:45 +0100

    feat: run several scenarios (#10)

---
+++ examples/ex4.md
@@ -0,0 +1,1 @@
+This script is used to test a scenarios file with several scenarios, all passing.
--- pkg/run/scenarios.go
+++ pkg/run/scenarios.go
@@ -20,83 +20,81 @@ import (
 )
 
 func Scenarios(file io.Reader, scenariosFile io.Reader, singleScenarioID *string) []api.ScenarioResult {
-	if singleScenarioID == nil {
-		log.Fatal("single scenario ID is required")
-	}
-
-	var scenarioResult = api.ScenarioResult{
-		ScenarioID: *singleScenarioID,
-	}
-
 	script, _ := syntax.NewParser().Parse(file, "")
-
-	var expectations *scenarios.Expect
-
-	var runnerOptions []interp.RunnerOption = []interp.RunnerOption{
-		interp.Env(expand.ListEnviron("GLOBAL=global_value")),
-		interp.Env(expand.ListEnviron(os.Environ()...)),
-	}
-
-	execHandlers := []func(next interp.ExecHandlerFunc) interp.ExecHandlerFunc{}
 
 	mocksDefinitions, err := scenarios.ParseScenarios(scenariosFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var scenarioResults []api.ScenarioResult
 	var found bool = false
 	for _, scenario := range mocksDefinitions.Scenarios {
-		if scenario.ID != *singleScenarioID {
+		var expectations *scenarios.Expect
+
+		var runnerOptions []interp.RunnerOption = []interp.RunnerOption{
+			interp.Env(expand.ListEnviron("GLOBAL=global_value")),
+			interp.Env(expand.ListEnviron(os.Environ()...)),
+		}
+
+		execHandlers := []func(next interp.ExecHandlerFunc) interp.ExecHandlerFunc{}
+
+		if singleScenarioID != nil && scenario.ID != *singleScenarioID {
 			continue
 		}
+		scenarioResult := api.ScenarioResult{
+			ScenarioID: scenario.ID,
+		}
+
 		for _, mock := range scenario.Mocks {
 			execHandlers = append(execHandlers, exec.GetExecHandler(mock))
 		}
 		expectations = scenario.Expect
 		found = true
-		break
+
+		runnerOptions = append(runnerOptions, interp.ExecHandlers(execHandlers...))
+
+		var callsResult call.CallsResult
+
+		var stdout io.ReadWriter = os.Stdout
+		var stderr io.ReadWriter = os.Stderr
+		if expectations != nil {
+			var stdoutBuffer bytes.Buffer
+			var stderrBuffer bytes.Buffer
+			stdout = &stdoutBuffer
+			stderr = &stderrBuffer
+
+			if len(expectations.Calls) > 0 {
+				var callHandler interp.CallHandlerFunc
+				callHandler, callsResult = call.GetCallHandler(expectations.Calls)
+				runnerOptions = append(runnerOptions, interp.CallHandler(callHandler))
+			}
+		}
+		runnerOptions = append(runnerOptions, interp.StdIO(nil, stdout, stderr))
+
+		runner, _ := interp.New(runnerOptions...)
+		result := runner.Run(context.TODO(), script)
+
+		var intResult int
+		if _, ok := result.(interp.ExitStatus); ok {
+			intResult = int(result.(interp.ExitStatus))
+		} else {
+			intResult = 0
+		}
+		if expectations == nil {
+			system.Exit(intResult)
+			return nil
+		} else {
+			expect.CheckExpectations(expectations, &scenarioResult, intResult, stdout, stderr)
+			if len(expectations.Calls) > 0 {
+				callsResult.CheckResults(&scenarioResult)
+			}
+		}
+		scenarioResults = append(scenarioResults, scenarioResult)
 	}
 
 	if !found {
 		log.Fatalf("scenario %s not found", *singleScenarioID)
 	}
-
-	runnerOptions = append(runnerOptions, interp.ExecHandlers(execHandlers...))
-
-	var callsResult call.CallsResult
-
-	var stdout io.ReadWriter = os.Stdout
-	var stderr io.ReadWriter = os.Stderr
-	if expectations != nil {
-		var stdoutBuffer bytes.Buffer
-		var stderrBuffer bytes.Buffer
-		stdout = &stdoutBuffer
-		stderr = &stderrBuffer
-
-		if len(expectations.Calls) > 0 {
-			var callHandler interp.CallHandlerFunc
-			callHandler, callsResult = call.GetCallHandler(expectations.Calls)
-			runnerOptions = append(runnerOptions, interp.CallHandler(callHandler))
-		}
-	}
-	runnerOptions = append(runnerOptions, interp.StdIO(nil, stdout, stderr))
-
-	runner, _ := interp.New(runnerOptions...)
-	result := runner.Run(context.TODO(), script)
-
-	var intResult int
-	if _, ok := result.(interp.ExitStatus); ok {
-		intResult = int(result.(interp.ExitStatus))
-	} else {
-		intResult = 0
-	}
-	if expectations == nil {
-		system.Exit(intResult)
-		return nil
-	} else {
-		expect.CheckExpectations(expectations, &scenarioResult, intResult, stdout, stderr)
-		if len(expectations.Calls) > 0 {
-			callsResult.CheckResults(&scenarioResult)
-		}
-		return []api.ScenarioResult{scenarioResult}
-	}
+	return scenarioResults
 }
